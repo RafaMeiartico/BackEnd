cadc37fe023a5e23048ead4123237a1a015a85a2
Rafaela Artico <rafameiartico@gmail.com>
2025-10-24 13:18:46 -0300

    Adicionando novos exercicios

--- ReviewArrays/Program.cs
+++ ReviewArrays/Program.cs
@@ -1,1 +1,12 @@
-﻿git
+﻿string [] nomes = new string[4]
+
+nomes [0] = "Edu";
+nomes [1] = "Márcio";
+nomes[2] = "Manoel";
+
+Console.WriteLine($"1º Nome: {nomes[0]}");
+Console.WriteLine($"2º Nome: {nomes[1]}");
+Console.WriteLine($"3º Nome: {nomes[2]}");
+Console.WriteLine($"4º Nome: {nomes[3]}");
+Console.WriteLine($"4º Nome: {nomes[3]}");
+Console.WriteLine($"5º Nome: {nomes[5]}");
